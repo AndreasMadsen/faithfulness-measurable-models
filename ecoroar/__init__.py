80cd1656bb07a5f99206db6af8c90992a9daee51
Andreas Madsen <amwebdk@gmail.com>
2022-02-07 22:33:44 -0500

    implement AdamW with warm-up-warm-down

--- ecoroar/__init__.py
+++ ecoroar/__init__.py
@@ -1,15 +1,18 @@
 
 __all__ = [
     'dataset', 'explain', 'metric',
-    'model', 'tokenizer', 'transform',
-    'types', 'util'
+    'model', 'optimizer', 'tokenizer',
+    'scheduler', 'transform', 'types',
+    'util',
 ]
 
 from . import dataset
 from . import explain
 from . import metric
 from . import model
+from . import optimizer
 from . import tokenizer
+from . import scheduler
 from . import transform
 from . import types
 from . import util
