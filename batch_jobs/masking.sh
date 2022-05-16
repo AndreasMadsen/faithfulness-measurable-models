1d5e579d769e733e03a9a6bd87ac1d25e2e55b7a
Andreas Madsen <amwebdk@gmail.com>
2022-05-16 14:52:03 -0400

    simpify job submission

--- batch_jobs/masking.sh
+++ batch_jobs/masking.sh
@@ -4,12 +4,14 @@ source "batch_jobs/_job_script.sh"
 seeds="0 1 2 3 4"
 
 # Actual   time="0:??:0"
-declare -r time="0:22:0"
+declare -A time=( ["IMDB"]="0:22:0" ["MultiNLI"]="0:30:0")
 
-for max_masking_ratio in {0..100..5}
-do
-    submit_seeds ${time} "$seeds" "masking-effect/imdb_s-%s_m-${max_masking_ratio}.json" \
-        $(job_script gpu) \
-        experiments/imdb_masking.py \
-        --max-masking-ratio "${max_masking_ratio}"
+for dataset in 'IMDB' 'MultiNLP'
+    for max_masking_ratio in {0..100..5}
+    do
+        submit_seeds "${time[$dataset]}" "$seeds" $(job_script gpu) \
+            experiments/masking.py \
+            --dataset "${dataset}" \
+            --max-masking-ratio "${max_masking_ratio}"
+    done
 done
