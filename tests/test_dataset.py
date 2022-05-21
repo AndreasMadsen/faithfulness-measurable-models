fcc0ef70ca3fa637f2b9baff3efca9da9b946ad2
Andreas Madsen <amwebdk@gmail.com>
2022-05-21 13:59:25 -0400

    refactor dataset tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,57 +1,32 @@
 
 import pathlib
+from dataclasses import dataclass
+
+import pytest
+
 from ecoroar.dataset import datasets
 
+@dataclass
+class DatasetSize:
+    name: str
+    train: int
+    valid: int
+    test: int
 
-def test_boolq_info():
-    dataset = datasets['BoolQ'](persistent_dir=pathlib.Path('.'))
+expected_sizes = [
+    DatasetSize('BoolQ', 9427, 3270, 3245),
+    DatasetSize('CoLA', 8551, 1043, 1063),
+    DatasetSize('IMDB', 20000, 5000, 25000),
+    DatasetSize('MNLI', 392702, 9815, 9796),
+    DatasetSize('QQP', 363846, 40430, 390965),
+    DatasetSize('SST2', 67349, 872, 1821),
+]
 
-    assert dataset.name == 'BoolQ'
-    assert dataset.train_num_examples == 9427
-    assert dataset.valid_num_examples == 3270
-    assert dataset.test_num_examples == 3245
+@pytest.mark.parametrize("info", expected_sizes, ids=lambda info: info.name)
+def test_dataset_size(info):
+    dataset = datasets[info.name](persistent_dir=pathlib.Path('.'))
 
-
-def test_cola_info():
-    dataset = datasets['CoLA'](persistent_dir=pathlib.Path('.'))
-
-    assert dataset.name == 'CoLA'
-    assert dataset.train_num_examples == 8551
-    assert dataset.valid_num_examples == 1043
-    assert dataset.test_num_examples == 1063
-
-
-def test_imdb_info():
-    dataset = datasets['IMDB'](persistent_dir=pathlib.Path('.'))
-
-    assert dataset.name == 'IMDB'
-    assert dataset.train_num_examples == 20000
-    assert dataset.valid_num_examples == 5000
-    assert dataset.test_num_examples == 25000
-
-
-def test_mnli_info():
-    dataset = datasets['MNLI'](persistent_dir=pathlib.Path('.'))
-
-    assert dataset.name == 'MNLI'
-    assert dataset.train_num_examples == 392702
-    assert dataset.valid_num_examples == 9815
-    assert dataset.test_num_examples == 9796
-
-
-def test_qqp_info():
-    dataset = datasets['QQP'](persistent_dir=pathlib.Path('.'))
-
-    assert dataset.name == 'QQP'
-    assert dataset.train_num_examples == 363846
-    assert dataset.valid_num_examples == 40430
-    assert dataset.test_num_examples == 390965
-
-
-def test_sst2_info():
-    dataset = datasets['SST2'](persistent_dir=pathlib.Path('.'))
-
-    assert dataset.name == 'SST2'
-    assert dataset.train_num_examples == 67349
-    assert dataset.valid_num_examples == 872
-    assert dataset.test_num_examples == 1821
+    assert dataset.name == info.name
+    assert dataset.train_num_examples == info.train
+    assert dataset.valid_num_examples == info.valid
+    assert dataset.test_num_examples == info.test
