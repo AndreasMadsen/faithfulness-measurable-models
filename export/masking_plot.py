219ced36ee76b0144a527397e29782e79a7aa5d1
Andreas Madsen <amwebdk@gmail.com>
2022-05-23 19:43:10 -0400

    update plotting script to multiple datasets

--- export/masking_plot.py
+++ export/masking_plot.py
@@ -8,8 +8,10 @@ import os.path as path
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-import scipy
+import scipy.stats
 import plotnine as p9
+
+from ecoroar.dataset import datasets
 
 
 def select_test_metric(partial_df):
@@ -21,29 +23,27 @@ def select_test_metric(partial_df):
 
 def ratio_confint(column_names):
     """Implementes a ratio-confidence interval
-
-    The idea is to project to logits space, then assume a normal distribution,
-    and then project back to the inital space.
-
+    This one uses bootstrapping.
     Method proposed here: https://stats.stackexchange.com/questions/263516
     """
     def agg(partial_df):
         summary = dict()
+        partial_df = partial_df.reset_index()
 
         for column_name in column_names:
-            x = partial_df.loc[:, column_name]
-            logits = scipy.special.logit(x)
-            mean = np.mean(logits)
-            sem = scipy.stats.sem(logits)
-            lower, upper = scipy.stats.t.interval(0.95, len(x) - 1,
-                                                  loc=mean,
-                                                  scale=sem)
+            x = partial_df.loc[:, column_name].to_numpy()
+            mean = np.mean(x)
 
-            lower = scipy.special.expit(lower)
-            mean = scipy.special.expit(mean)
-            upper = scipy.special.expit(upper)
-            if np.isnan(sem) or sem == 0:
-                lower, upper = mean, mean
+            if np.all(x[0] == x):
+                lower = mean
+                upper = mean
+            else:
+                res = scipy.stats.bootstrap(
+                    (x, ), np.mean,
+                    confidence_level=0.95, random_state=np.random.default_rng(0)
+                )
+                lower = res.confidence_interval.low
+                upper = res.confidence_interval.high
 
             summary.update({
                 f'{column_name}_lower': lower,
@@ -53,7 +53,6 @@ def ratio_confint(column_names):
             })
 
         return pd.Series(summary)
-
     return agg
 
 
@@ -76,13 +75,14 @@ if __name__ == "__main__":
     args, unknown = parser.parse_known_args()
 
     dataset_mapping = pd.DataFrame([
-        {'dataset': 'imdb', 'dataset_pretty': 'IMDB', 'test_metric': 'macro_f1'}
+        { 'dataset': dataset._name, 'test_metric': dataset._early_stopping_metric }
+        for dataset in datasets.values()
     ])
 
     if args.stage in ['both', 'preprocess']:
         # Read JSON files into dataframe
         results = []
-        files = glob.glob(f'{args.persistent_dir}/results/masking-effect/*.json')
+        files = glob.glob(f'{args.persistent_dir}/results/masking_*.json')
         for file in tqdm(files, desc='Loading .json files'):
             with open(file, 'r') as fp:
                 try:
@@ -94,28 +94,28 @@ if __name__ == "__main__":
         # Compute confint and mean for each group
         df = (df
               .merge(dataset_mapping, on='dataset')
-              .drop(['dataset'], axis=1)
-              .groupby(['seed', 'dataset_pretty', 'max_masking_ratio'])
+              .groupby(['seed', 'dataset', 'max_masking_ratio'])
               .apply(select_test_metric))
 
     if args.stage in ['preprocess']:
         os.makedirs(f'{args.persistent_dir}/pandas', exist_ok=True)
-        df.to_pickle(f'{args.persistent_dir}/pandas/masking-effect.pd.pkl.xz')
+        df.to_pickle(f'{args.persistent_dir}/pandas/masking.pd.pkl.xz')
     elif args.stage in ['plot']:
-        df = pd.read_pickle(f'{args.persistent_dir}/pandas/masking-effect.pd.pkl.xz')
+        df = pd.read_pickle(f'{args.persistent_dir}/pandas/masking.pd.pkl.xz')
 
     if args.stage in ['both', 'plot']:
         # Generate result plots
-        df_plot = df.groupby(['dataset_pretty', 'max_masking_ratio']).apply(ratio_confint(['metric']))
+        df_plot = df.groupby(['dataset', 'max_masking_ratio']).apply(ratio_confint(['metric']))
 
         # Generate plot
         p = (p9.ggplot(df_plot.reset_index(), p9.aes(x='max_masking_ratio'))
-             + p9.geom_ribbon(p9.aes(ymin='metric_lower', ymax='metric_upper', fill='dataset_pretty'), alpha=0.35)
-             + p9.geom_line(p9.aes(y='metric_mean', color='dataset_pretty'))
-             + p9.geom_point(p9.aes(y='metric_mean', color='dataset_pretty', shape='dataset_pretty'))
-             + p9.geom_jitter(p9.aes(y='metric', group='seed', shape='dataset_pretty'),
+             + p9.geom_ribbon(p9.aes(ymin='metric_lower', ymax='metric_upper', fill='dataset'), alpha=0.35)
+             + p9.geom_line(p9.aes(y='metric_mean', color='dataset'))
+             + p9.geom_point(p9.aes(y='metric_mean', color='dataset', shape='dataset'))
+             + p9.geom_jitter(p9.aes(y='metric', group='seed', shape='dataset'),
                               color='black', alpha=0.3, width=1, data=df.reset_index())
-             + p9.labs(y='Macro F1', shape='', x='Max masking ratio')
+             + p9.facet_grid("dataset ~", scales="free_y")
+             + p9.labs(y='Performance', shape='', x='Max masking ratio')
              + p9.scale_y_continuous(labels=lambda ticks: [f'{tick:.0%}' for tick in ticks])
              + p9.scale_x_continuous(labels=lambda ticks: [f'{tick:.0f}%' for tick in ticks])
              + p9.scale_color_discrete(guide=False)
@@ -124,5 +124,5 @@ if __name__ == "__main__":
 
         # Save plot, the width is the \linewidth of a collumn in the LaTeX document
         os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
-        p.save(f'{args.persistent_dir}/plots/imdb-masking.pdf', width=6.30045 + 0.2, height=3, units='in')
-        p.save(f'{args.persistent_dir}/plots/imdb-masking.png', width=6.30045 + 0.2, height=3, units='in')
+        p.save(f'{args.persistent_dir}/plots/imdb-masking.pdf', width=6.30045 + 0.2, height=7, units='in')
+        p.save(f'{args.persistent_dir}/plots/imdb-masking.png', width=6.30045 + 0.2, height=7, units='in')
