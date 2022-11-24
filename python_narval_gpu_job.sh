2d597d3b9e77a2be13e23bcc16e60b7ed01ba3ee
Andreas Madsen <amwebdk@gmail.com>
2022-11-24 13:01:46 -0500

    upgrade python version

--- python_narval_gpu_job.sh
+++ python_narval_gpu_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=2:00:00
 
 # Load modules
-module load python/3.8.10 gcc/9.3.0 cuda/11.4 cudnn/8.2.0
+module load python/3.10.2 gcc/9.3.0 git-lfs/2.11.0 cuda/11.4 cudnn/8.2.0
 
 # Create environment
 virtualenv --app-data $SCRATCH/virtualenv --no-download $SLURM_TMPDIR/env
@@ -14,7 +14,7 @@ source $SLURM_TMPDIR/env/bin/activate
 
 # Install project
 cd $HOME/workspace/economical-roar
-python -m pip install --no-index -U pip
+python -m pip install --no-index -U pip setuptools
 python -m pip install --no-index --find-links $HOME/python_wheels -e .
 
 # Enable offline model
