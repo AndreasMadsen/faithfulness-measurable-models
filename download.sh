a4a47d1ab192b524d0117b77bba9e9d83650bc16
Andreas Madsen <amwebdk@gmail.com>
2022-11-24 13:00:47 -0500

    use custom transformers package
    This is being upstreamed to the offical transformers package

--- download.sh
+++ download.sh
@@ -1,20 +1,31 @@
 # Load modules
-module load python/3.8.10 gcc/9.3.0
+module load python/3.10.2 gcc/9.3.0 git-lfs/2.11.0
 
 # Create environment
 TMP_ENV=$(mktemp -d)
 virtualenv --app-data $SCRATCH/virtualenv --no-download $TMP_ENV/env
 source $TMP_ENV/env/bin/activate
+python -m pip install --no-index -U pip setuptools
+python -m pip install -U build wheel
 
 # Download package dependencies
 mkdir -p $HOME/python_wheels
+
+# build transformers
+# This private build is being upstreamed at https://github.com/huggingface/transformers/pull/20305
+cd $(mktemp -d)
+git clone https://github.com/andreasmadsen/transformers.git
+cd transformers
+git checkout roberta-prelayernorm
+python -m build
+cp dist/*-py3-none-any.whl $HOME/python_wheels
+
+# install dependencies
 cd $HOME/python_wheels
-python -m pip download --no-deps 'transformers>=4.19.1'
-python -m pip download --no-deps 'tensorflow-datasets>=4.5.0'
+python -m pip download --no-deps 'huggingface-hub<1.0,>=0.10.0'
 
 # Install project
 cd $HOME/workspace/economical-roar
-python -m pip install --no-index -U pip
 python -m pip install --no-index --find-links $HOME/python_wheels -e .
 
 # Envs
