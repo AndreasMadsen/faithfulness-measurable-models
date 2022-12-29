d7c591651656b2800e5088f346732eaf967c98b4
Andreas Madsen <amwebdk@gmail.com>
2022-12-29 17:14:34 +0100

    update expected walltimes

--- batch_jobs/masking.sh
+++ batch_jobs/masking.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # jobs: 11
 source "batch_jobs/_job_script.sh"
-seeds="0"
+seeds="0 1 2 3 4"
+
+declare -A size=( ["roberta-sb"]="small" ["roberta-sl"]="small"
+                  ["roberta-m15"]="large" ["roberta-m20"]="large" ["roberta-m30"]="large" ["roberta-m40"]="large" ["roberta-m50"]="large" )
 
 # V100
-#declare -A time=( ["roberta-sb BoolQ"]="0:18:0"  ["roberta-sb CoLA"]="0:17:0"  ["roberta-sb IMDB"]="0:41:0"  ["roberta-sb QQP"]="5:20:0"  ["roberta-sb SST2"]="1:00:0"
-#                  ["roberta-sl BoolQ"]="0:31:0"  ["roberta-sl CoLA"]="0:28:0"  ["roberta-sl IMDB"]="1:29:0"  ["roberta-sl QQP"]="10:00:0"  ["roberta-sl SST2"]="1:57:0"
-#                  ["roberta-m40 BoolQ"]="0:31:0" ["roberta-m40 CoLA"]="0:28:0" ["roberta-m40 IMDB"]="1:29:0" ["roberta-m40 QQP"]="10:00:0" ["roberta-m40 SST2"]="1:57:0" )
+#declare -A time=( ["small BoolQ"]="0:15:0" ["small CoLA"]="0:08:0" ["small IMDB"]="0:39:0" ["small QQP"]="1:40:0" ["small SST2"]="0:21:0"
+#                  ["large BoolQ"]="0:27:0" ["large CoLA"]="0:13:0" ["large IMDB"]="1:24:0" ["large QQP"]="2:45:0" ["large SST2"]="0:42:0" )
+declare -A time=(  ["small BoolQ"]="0:30:0" ["small CoLA"]="0:30:0" ["small IMDB"]="1:15:0" ["small QQP"]="2:50:0" ["small SST2"]="0:50:0"
+                   ["large BoolQ"]="0:50:0" ["large CoLA"]="0:40:0" ["large IMDB"]="3:00:0" ["large QQP"]="4:00:0" ["large SST2"]="1:10:0" )
 
-declare -A time=( ["roberta-sb BoolQ"]="0:30:0"  ["roberta-sb CoLA"]="0:30:0"  ["roberta-sb IMDB"]="1:00:0"  ["roberta-sb QQP"]="6:00:0"  ["roberta-sb SST2"]="1:20:0"
-                  ["roberta-sl BoolQ"]="0:50:0"  ["roberta-sl CoLA"]="0:50:0"  ["roberta-sl IMDB"]="2:00:0"  ["roberta-sl QQP"]="10:00:0"  ["roberta-sl SST2"]="2:30:0"
-                  ["roberta-m40 BoolQ"]="0:50:0" ["roberta-m40 CoLA"]="0:50:0" ["roberta-m40 IMDB"]="2:00:0" ["roberta-m40 QQP"]="10:00:0" ["roberta-m40 SST2"]="2:30:0" )
-
-for model in 'roberta-sb' 'roberta-sl' 'roberta-m40'
+for model in 'roberta-sb' 'roberta-sl' 'roberta-m15' 'roberta-m20' 'roberta-m30' 'roberta-m40' 'roberta-m50'
 do
     for dataset in 'BoolQ' 'CoLA' 'IMDB' 'QQP' 'SST2'
     do
-        for max_masking_ratio in {0..100..5}
+        for max_masking_ratio in {0..100..20}
         do
-            submit_seeds "${time[$model $dataset]}" "$seeds" $(job_script gpu) \
+            submit_seeds "${time[${size[$model]} $dataset]}" "$seeds" $(job_script gpu) \
                 experiments/masking.py \
                 --model "${model}" \
                 --dataset "${dataset}" \
--- ecoroar/util/default_args.py
+++ ecoroar/util/default_args.py
@@ -1,9 +1,11 @@
 
 # CoLA, disable JIT compile. Small dataset, too slow to JIT compile.
 # BoolQ, disable JIT compile. Memory allocation issue, graph to big?
+# SST2, disable JIT compile. Small dataset, slightly slower to JIT compile.
+# IMDB, disable JIT compile. Small dataset, slightly slower to JIT compile.
 
 def default_jit_compile(args):
     if args.jit_compile is not None:
         return args.jit_compile
 
-    return args.dataset not in ['CoLA', 'BoolQ']
+    return args.dataset not in ['CoLA', 'BoolQ', 'SST2', 'IMDB']
