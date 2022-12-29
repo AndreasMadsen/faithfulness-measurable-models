368c21d8547bafc412f5cf5c3116e72fd7d85db9
Andreas Madsen <amwebdk@gmail.com>
2022-12-29 17:18:11 +0100

    update plot script to new output format

--- export/masking_plot.py
+++ export/masking_plot.py
@@ -17,7 +17,7 @@ from ecoroar.dataset import datasets
 def select_test_metric(partial_df):
     column_name = partial_df.loc[:, 'test_metric'].iat[0]
     return pd.Series({
-        'metric': partial_df.loc[:, column_name].iat[0]
+        'metric': partial_df.loc[:, f'results.{column_name}'].iat[0]
     })
 
 
@@ -74,9 +74,19 @@ if __name__ == "__main__":
     pd.set_option('display.max_rows', None)
     args, unknown = parser.parse_known_args()
 
+    # TODO: figure out why only loss and accuarcy are logged by .evalaute
     dataset_mapping = pd.DataFrame([
-        { 'dataset': dataset._name, 'test_metric': dataset._early_stopping_metric }
+        { 'args.dataset': dataset._name, 'test_metric': 'accuracy' } # dataset._early_stopping_metric }
         for dataset in datasets.values()
+    ])
+    model_mapping = pd.DataFrame([
+        { 'args.model': 'roberta-m15', 'model_category': 'masking-ratio' },
+        { 'args.model': 'roberta-m20', 'model_category': 'masking-ratio' },
+        { 'args.model': 'roberta-m30', 'model_category': 'masking-ratio' },
+        { 'args.model': 'roberta-m40', 'model_category': 'masking-ratio' },
+        { 'args.model': 'roberta-m50', 'model_category': 'masking-ratio' },
+        { 'args.model': 'roberta-sb', 'model_category': 'size' },
+        { 'args.model': 'roberta-sl', 'model_category': 'size' }
     ])
 
     if args.stage in ['both', 'preprocess']:
@@ -89,13 +99,20 @@ if __name__ == "__main__":
                     results.append(json.load(fp))
                 except json.decoder.JSONDecodeError:
                     print(f'{file} has a format error')
-        df = pd.DataFrame(results)
 
-        # Compute confint and mean for each group
+        df = pd.json_normalize(results).explode('results', ignore_index=True)
+        results = pd.json_normalize(df.pop('results')).add_prefix('results.')
+        df = pd.concat([df, results], axis=1)
+
+        # Select test metric
         df = (df
-              .merge(dataset_mapping, on='dataset')
-              .groupby(['seed', 'dataset', 'max_masking_ratio'])
-              .apply(select_test_metric))
+              .merge(dataset_mapping, on='args.dataset')
+              .merge(model_mapping, on='args.model')
+              .groupby(['args.model', 'args.seed', 'args.dataset', 'args.max_masking_ratio',
+                        'results.masking_ratio',
+                        'model_category'])
+              .apply(select_test_metric)
+              .reset_index())
 
     if args.stage in ['preprocess']:
         os.makedirs(f'{args.persistent_dir}/pandas', exist_ok=True)
@@ -104,25 +121,30 @@ if __name__ == "__main__":
         df = pd.read_pickle(f'{args.persistent_dir}/pandas/masking.pd.pkl.xz')
 
     if args.stage in ['both', 'plot']:
-        # Generate result plots
-        df_plot = df.groupby(['dataset', 'max_masking_ratio']).apply(ratio_confint(['metric']))
+        # Compute confint and mean for each group
 
-        # Generate plot
-        p = (p9.ggplot(df_plot.reset_index(), p9.aes(x='max_masking_ratio'))
-             + p9.geom_ribbon(p9.aes(ymin='metric_lower', ymax='metric_upper', fill='dataset'), alpha=0.35)
-             + p9.geom_line(p9.aes(y='metric_mean', color='dataset'))
-             + p9.geom_point(p9.aes(y='metric_mean', color='dataset', shape='dataset'))
-             + p9.geom_jitter(p9.aes(y='metric', group='seed', shape='dataset'),
-                              color='black', alpha=0.3, width=1, data=df.reset_index())
-             + p9.facet_grid("dataset ~", scales="free_y")
-             + p9.labs(y='Performance', shape='', x='Max masking ratio')
-             + p9.scale_y_continuous(labels=lambda ticks: [f'{tick:.0%}' for tick in ticks])
-             + p9.scale_x_continuous(labels=lambda ticks: [f'{tick:.0f}%' for tick in ticks])
-             + p9.scale_color_discrete(guide=False)
-             + p9.scale_fill_discrete(guide=False)
-             + p9.scale_shape_discrete(guide=False))
+        for model_category in ['masking-ratio', 'size']:
+            df_subset = df.loc[(df['results.masking_ratio'] == 0.0) & (df['model_category'] == model_category), :]
+            df_plot = (df_subset
+                    .groupby(['args.model', 'args.dataset', 'args.max_masking_ratio'])
+                    .apply(ratio_confint(['metric']))
+                    .reset_index()
+            )
 
-        # Save plot, the width is the \linewidth of a collumn in the LaTeX document
-        os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
-        p.save(f'{args.persistent_dir}/plots/imdb-masking.pdf', width=6.30045 + 0.2, height=7, units='in')
-        p.save(f'{args.persistent_dir}/plots/imdb-masking.png', width=6.30045 + 0.2, height=7, units='in')
+            # Generate plot
+            p = (p9.ggplot(df_plot, p9.aes(x='args.max_masking_ratio'))
+                + p9.geom_ribbon(p9.aes(ymin='metric_lower', ymax='metric_upper', fill='args.model'), alpha=0.35)
+                + p9.geom_line(p9.aes(y='metric_mean', color='args.model'))
+                + p9.geom_point(p9.aes(y='metric_mean', color='args.model', shape='args.model'))
+                + p9.geom_jitter(p9.aes(y='metric', group='args.seed', shape='args.dataset', color='args.model'),
+                                alpha=0.3, width=1, data=df_subset)
+                + p9.facet_grid("args.dataset ~", scales="free_y")
+                + p9.labs(y='Performance', shape='', x='Max masking ratio')
+                + p9.scale_y_continuous(labels=lambda ticks: [f'{tick:.0%}' for tick in ticks])
+                + p9.scale_x_continuous(labels=lambda ticks: [f'{tick:.0f}%' for tick in ticks])
+                + p9.scale_shape_discrete(guide=False))
+
+            # Save plot, the width is the \linewidth of a collumn in the LaTeX document
+            os.makedirs(f'{args.persistent_dir}/plots', exist_ok=True)
+            p.save(f'{args.persistent_dir}/plots/masking_max-ratio_m-{model_category}.pdf', width=6.30045 + 0.2, height=7, units='in')
+            p.save(f'{args.persistent_dir}/plots/masking_max-ratio_m-{model_category}.png', width=6.30045 + 0.2, height=7, units='in')
