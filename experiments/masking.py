use fixed masking for test performance

experiments/masking.py
@@ -12,7 +12,7 @@ from ecoroar.util import generate_experiment_id, model_name_to_huggingface_repo,
 from ecoroar.dataset import datasets
 from ecoroar.tokenizer import HuggingfaceTokenizer
 from ecoroar.model import HuggingfaceModel
-from ecoroar.transform import RandomMasking, BucketedPaddedBatch
+from ecoroar.transform import RandomFixedMasking, RandomMaxMasking, BucketedPaddedBatch
 from ecoroar.optimizer import AdamW
 from ecoroar.scheduler import LinearSchedule
 
@@ -130,7 +130,7 @@ if __name__ == '__main__':
         batcher = lambda batch_size, padding_values, num_parallel_calls: \
             lambda dataset: dataset.padded_batch(batch_size, padding_values=padding_values)
 
-    masker_train = RandomMasking(args.max_masking_ratio / 100, tokenizer, seed=args.seed)
+    masker_train = RandomMaxMasking(args.max_masking_ratio / 100, tokenizer, seed=args.seed)
     dataset_train_batched = dataset_train \
         .shuffle(dataset.train_num_examples, seed=args.seed) \
         .map(lambda x, y: (masker_train(x), y), num_parallel_calls=tf.data.AUTOTUNE) \
@@ -162,8 +162,9 @@ if __name__ == '__main__':
     checkpoint_dir = tempfile.mkdtemp()
     tensorboard_dir = tempfile.mkdtemp()
 
+    # Train models and collect validation performance at each epoch
     train_time_start = timer()
-    model.fit(dataset_train_batched, validation_data=dataset_valid_batched, epochs=args.max_epochs, callbacks=[
+    history = model.fit(dataset_train_batched, validation_data=dataset_valid_batched, epochs=args.max_epochs, callbacks=[
         tf.keras.callbacks.ModelCheckpoint(
             filepath=checkpoint_dir,
             monitor=dataset.early_stopping_metric, mode='max',
@@ -177,9 +178,10 @@ if __name__ == '__main__':
     ])
     durations['train_time'] = timer() - train_time_start
 
+    # Evalute test performance at different masking ratios
     results_test = []
     for test_masking_ratio in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-        masker_test = RandomMasking(test_masking_ratio, tokenizer, seed=args.seed)
+        masker_test = RandomFixedMasking(test_masking_ratio, tokenizer, seed=args.seed)
         dataset_test_batched = dataset_test \
             .map(lambda x, y: (masker_test(x), y), num_parallel_calls=tf.data.AUTOTUNE) \
             .apply(batcher(args.batch_size,

tests/test_transform_random_fixed_masking.py
@@ -4,7 +4,7 @@ import pathlib
 import numpy as np
 
 from ecoroar.tokenizer import HuggingfaceTokenizer
-from ecoroar.transform import RandomMasking
+from ecoroar.transform import RandomFixedMasking
 
 
 def test_masking_special_tokens_kept():
@@ -12,7 +12,7 @@ def test_masking_special_tokens_kept():
     output = tokenizer(("This was an absolutely terrible movie.", ))
 
     for seed in range(100):
-        masker_max = RandomMasking(1.0, tokenizer, seed=0)
+        masker_max = RandomFixedMasking(1.0, tokenizer, seed=0)
 
         np.testing.assert_array_equal(output['input_ids'].numpy(),
                                       [0, 713, 21, 41, 3668, 6587, 1569, 4, 2])
@@ -29,7 +29,7 @@ def test_masking_zero():
     output = tokenizer(("This was an absolutely terrible movie.", ))
 
     for seed in range(100):
-        masker_none = RandomMasking(0.0, tokenizer, seed=0)
+        masker_none = RandomFixedMasking(0.0, tokenizer, seed=0)
 
         np.testing.assert_array_equal(output['input_ids'].numpy(),
                                       [0, 713, 21, 41, 3668, 6587, 1569, 4, 2])
@@ -40,12 +40,24 @@ def test_masking_zero():
 
 def test_masking_some():
     tokenizer = HuggingfaceTokenizer('roberta-base', persistent_dir=pathlib.Path('.'))
-    output = tokenizer(("This was an absolutely terrible movie.", ))
-    masker_some = RandomMasking(0.5, tokenizer, seed=1)
+    output = tokenizer(("This was truely an absolutely - terrible movie.", ))
     mask = tokenizer.mask_token_id.numpy()
 
     np.testing.assert_array_equal(output['input_ids'].numpy(),
-                                  [0, 713, 21, 41, 3668, 6587, 1569, 4, 2])
+                                  [0, 713, 21, 1528, 352, 41, 3668, 111, 6587, 1569, 4, 2])
 
-    np.testing.assert_array_equal(masker_some(output)['input_ids'].numpy(),
-                                  [0, mask, 21, 41, mask, 6587, 1569, mask, 2])
+    masker_20 = RandomFixedMasking(0.2, tokenizer, seed=1)
+    np.testing.assert_array_equal(masker_20(output)['input_ids'].numpy(),
+                                  [0, mask, 21, 1528, 352, 41, 3668, mask, 6587, 1569, 4, 2])
+
+    masker_40 = RandomFixedMasking(0.4, tokenizer, seed=1)
+    np.testing.assert_array_equal(masker_40(output)['input_ids'].numpy(),
+                                  [0, mask, 21, 1528, 352, 41, mask, mask, 6587, 1569, mask, 2])
+
+    masker_60 = RandomFixedMasking(0.6, tokenizer, seed=1)
+    np.testing.assert_array_equal(masker_60(output)['input_ids'].numpy(),
+                                  [0, mask, 21, mask, 352, 41, mask, mask, mask, 1569, mask, 2])
+
+    masker_80 = RandomFixedMasking(0.8, tokenizer, seed=1)
+    np.testing.assert_array_equal(masker_80(output)['input_ids'].numpy(),
+                                  [0, mask, 21, mask, mask, 41, mask, mask, mask, mask, mask, 2])