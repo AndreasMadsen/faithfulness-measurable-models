fde315fc69236ccef9520299a06ca29106df0764
Andreas Madsen <amwebdk@gmail.com>
2023-01-26 13:16:19 -0500

    update times

--- batch_jobs/masking.sh
+++ batch_jobs/masking.sh
@@ -3,28 +3,30 @@
 source "batch_jobs/_job_script.sh"
 seeds="0 1 2 3 4"
 
-declare -A size=( ["roberta-sb"]="small" ["roberta-sl"]="small"
+declare -A size=( ["roberta-sb"]="small" ["roberta-sl"]="large"
                   ["roberta-m15"]="large" ["roberta-m20"]="large" ["roberta-m30"]="large" ["roberta-m40"]="large" ["roberta-m50"]="large" )
 
 # V100
-#                   20 epochs,              20 epochs,              3 epochs,               3 epochs,              3 epochs
-#declare -A time=( ["small BoolQ"]="3:20:0" ["small CoLA"]="1:12:0" ["small IMDB"]="0:39:0" ["small QQP"]="1:40:0" ["small SST2"]="0:21:0"
-#                  ["large BoolQ"]="8:20:0" ["large CoLA"]="2:40:0" ["large IMDB"]="1:24:0" ["large QQP"]="2:45:0" ["large SST2"]="0:42:0" )
-declare -A time=(  ["small BoolQ"]="4:00:0" ["small CoLA"]="1:40:0" ["small IMDB"]="1:15:0" ["small QQP"]="2:50:0" ["small SST2"]="0:50:0"
-                   ["large BoolQ"]="9:00:0" ["large CoLA"]="3:20:0" ["large IMDB"]="3:00:0" ["large QQP"]="4:00:0" ["large SST2"]="1:10:0" )
+#declare -A time=( ["small BoolQ"]="3:20:0" ["small CoLA"]="1:12:0" ["small IMDB"]="2:49:0" ["small QQP"]="?:??:0" ["small SST2"]="1:32:0"  ["small MNLI"]="9:11:0"
+#                  ["large BoolQ"]="8:20:0" ["large CoLA"]="2:40:0" ["large IMDB"]="7:10:0" ["large QQP"]="19:50:0" ["large SST2"]="3:07:0"  ["large MNLI"]="?:??:0"
+declare -A time=(  ["small BoolQ"]="4:00:0" ["small CoLA"]="1:40:0" ["small IMDB"]="3:30:0" ["small QQP"]="21:00:0" ["small SST2"]="2:00:0" ["small MNLI"]="9:11:0"
+                   ["large BoolQ"]="9:00:0" ["large CoLA"]="3:20:0" ["large IMDB"]="8:00:0" ["large QQP"]="21:00:0" ["large SST2"]="3:40:0" ["large MNLI"]="21:00:0" )
 
-for model in 'roberta-sb' 'roberta-sl' 'roberta-m15' 'roberta-m20' 'roberta-m30' 'roberta-m40' 'roberta-m50'
+for model in 'roberta-sb' 'roberta-sl' # 'roberta-m15' 'roberta-m20' 'roberta-m30' 'roberta-m40' 'roberta-m50'
 do
-    for dataset in 'BoolQ' 'CoLA'
+    for dataset in 'BoolQ' 'CoLA' 'SST2' 'IMDB' 'QQP' 'MNLI'
     do
-        for max_masking_ratio in {0..100..20}
+        for max_masking_ratio in 0 100
         do
-            submit_seeds "${time[${size[$model]} $dataset]}" "$seeds" $(job_script gpu) \
-                experiments/masking.py \
-                --model "${model}" \
-                --dataset "${dataset}" \
-                --max-masking-ratio "${max_masking_ratio}" \
-                --max-epochs 20
+            for seed in 0 1 2 3 4
+            do
+                submit_seeds "${time[${size[$model]} $dataset]}" "$seed" $(job_script gpu) \
+                    experiments/masking.py \
+                    --model "${model}" \
+                    --dataset "${dataset}" \
+                    --max-masking-ratio "${max_masking_ratio}" \
+                    --max-epochs 20
+            done
         done
     done
 done
