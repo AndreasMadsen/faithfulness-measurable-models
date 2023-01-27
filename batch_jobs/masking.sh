5526194c55a1a687b619ae6cebbd37f709c64113
Andreas Madsen <amwebdk@gmail.com>
2023-01-27 12:19:58 -0500

    update time estimates and epochs

--- batch_jobs/masking.sh
+++ batch_jobs/masking.sh
@@ -6,29 +6,29 @@ seeds="0 1 2 3 4"
 declare -A size=( ["roberta-sb"]="small" ["roberta-sl"]="large"
                   ["roberta-m15"]="large" ["roberta-m20"]="large" ["roberta-m30"]="large" ["roberta-m40"]="large" ["roberta-m50"]="large" )
 
-#                    V100                     V100                      epochs
-declare -A time=(  # ["small BoolQ"]="3:20:0" ["large BoolQ"]="8:20:0"  20
-                     ["small BoolQ"]="4:00:0" ["large BoolQ"]="9:00:0"
-                   # ["small CB"]="?:??:??"   ["large CB"]="?:??:??"    20
-                     ["small CB"]="5:00:00"   ["large CB"]="5:00:0"
-                   # ["small CoLA"]="1:12:0"  ["large CoLA"]="2:40:0"   20
-                     ["small CoLA"]="1:40:0"  ["large CoLA"]="3:20:0"
-                   # ["small IMDB"]="2:49:0"  ["large IMDB"]="7:10:0"   20
-                     ["small IMDB"]="3:30:0"  ["large IMDB"]="8:00:0"
-                   # ["small MNLI"]="9:11:0"  ["large MNLI"]="?:??:0"   20
-                     ["small MNLI"]="9:11:0"  ["large MNLI"]="21:00:0"
-                   # ["small MRPC"]="?:??:??" ["large MRPC"]="?:??:??"  20
-                     ["small MRPC"]="5:00:00" ["large MRPC"]="5:00:0"
-                   # ["small QNLI"]="?:??:??" ["large QNLI"]="?:??:??"  20
-                     ["small QNLI"]="5:00:00" ["large QNLI"]="5:00:0"
-                   # ["small QQP"]="?:??:0"   ["large QQP"]="19:50:0"   20
-                     ["small QQP"]="21:00:0"  ["large QQP"]="21:00:0"
-                   # ["small RTE"]="?:??:??"  ["large RTE"]="?:??:??"   20
-                     ["small RTE"]="5:00:00"  ["large RTE"]="5:00:0"
-                   # ["small SST2"]="1:32:0"  ["large SST2"]="3:07:0"   20
-                     ["small SST2"]="2:00:0"  ["large SST2"]="3:40:0"
-                   # ["small WNLI"]="?:??:??" ["large WNLI"]="?:??:??"  20
-                     ["small WNLI"]="5:00:00" ["large WNLI"]="5:00:0" )
+#                   V100                     V100                      epochs
+declare -A time=( # ["small BoolQ"]="3:10:0" ["large BoolQ"]="7:58:0"  15
+                    ["small BoolQ"]="4:00:0" ["large BoolQ"]="9:00:0"
+                  # ["small CB"]="?:??:?"    ["large CB"]="?:??:?"     20
+                    ["small CB"]="3:00:0"    ["large CB"]="3:00:0"
+                  # ["small CoLA"]="1:8:0"   ["large CoLA"]="2:33:0"   15
+                    ["small CoLA"]="1:40:0"  ["large CoLA"]="3:00:0"
+                  # ["small IMDB"]="1:38:0"  ["large IMDB"]="6:04:0"   10
+                    ["small IMDB"]="2:30:0"  ["large IMDB"]="7:00:0"
+                  # ["small MNLI"]="4:33:0"  ["large MNLI"]="9:53:0"   10
+                    ["small MNLI"]="5:11:0"  ["large MNLI"]="11:00:0"
+                  # ["small MRPC"]="0:??:0" ["large MRPC"]="?:??:?"    20
+                    ["small MRPC"]="3:00:0"  ["large MRPC"]="3:00:0"
+                  # ["small QNLI"]="?:??:?"  ["large QNLI"]="?:??:?"   20
+                    ["small QNLI"]="5:00:0"  ["large QNLI"]="11:00:0"
+                  # ["small QQP"]="3:01:0"   ["large QQP"]="8:43:0"    10
+                    ["small QQP"]="4:00:0"   ["large QQP"]="9:50:0"
+                  # ["small RTE"]="?:??:?"   ["large RTE"]="?:??:?"    20
+                    ["small RTE"]="3:00:0"   ["large RTE"]="3:00:0"
+                  # ["small SST2"]="0:48:0"  ["large SST2"]="1:35:0"   10
+                    ["small SST2"]="1:20:0"  ["large SST2"]="2:20:0"
+                  # ["small WNLI"]="?:??:?"  ["large WNLI"]="?:??:?"   20
+                    ["small WNLI"]="3:00:0"  ["large WNLI"]="3:00:0" )
 
 for model in 'roberta-sb' 'roberta-sl' # 'roberta-m15' 'roberta-m20' 'roberta-m30' 'roberta-m40' 'roberta-m50'
 do
@@ -42,8 +42,7 @@ do
                     experiments/masking.py \
                     --model "${model}" \
                     --dataset "${dataset}" \
-                    --max-masking-ratio "${max_masking_ratio}" \
-                    --max-epochs 20
+                    --max-masking-ratio "${max_masking_ratio}"
             done
         done
     done
