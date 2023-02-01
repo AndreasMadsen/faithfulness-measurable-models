eeae991c375be634daae9c3d75844034b0eceef6
Andreas Madsen <amwebdk@gmail.com>
2023-02-01 10:31:29 -0500

    fix experiment naming

--- experiments/experiment-name.py
+++ experiments/experiment-name.py
@@ -1,6 +1,6 @@
 import argparse
 
-from ecoroar.util import generate_experiment_id
+from ecoroar.util import generate_experiment_id, default_max_epochs
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed',
@@ -20,7 +20,7 @@ parser.add_argument('--dataset',
                     help='The dataset to fine-tune on')
 parser.add_argument('--max-epochs',
                     action='store',
-                    default=3,
+                    default=None,
                     type=int,
                     help='The max number of epochs to use')
 parser.add_argument('--max-masking-ratio',
@@ -31,6 +31,7 @@ parser.add_argument('--max-masking-ratio',
 
 if __name__ == '__main__':
     args, unknown = parser.parse_known_args()
+    args.max_epochs = default_max_epochs(args)
 
     experiment_id = generate_experiment_id(
         'masking',
