6c14bf29f75d4fdd1b79109729d86e027b91b87b
Andreas Madsen <amwebdk@gmail.com>
2023-02-22 17:19:49 -0500

    add embedding interface to huggingface models

--- ecoroar/model/huggingface.py
+++ ecoroar/model/huggingface.py
@@ -15,28 +15,55 @@ def silence_huggingface():
     yield
     transformers.logging.set_verbosity(level)
 
+def huggingface_constructor(config: AutoConfig):
+    """Returns a sequence classification class
 
-def HuggingfaceModel(model_name: str, persistent_dir: pathlib.Path, num_classes: int) -> Model:
+    Args:
+        config (AutoConfig): huggingface config object
+
+    Returns:
+        Model: SequenceClassification class
+    """
+    match config.model_type:
+        case 'roberta':
+            return ExtraRoBERTaForSequenceClassification
+
+    raise NotImplementedError(f'An embedding abstraction have been implemented for a {config.model_type} model')
+
+def huggingface_model_from_repo(repo: str, persistent_dir: pathlib.Path, num_classes: int) -> Model:
     """Creates a sequence classification model
 
     Args:
-        model_name (str): the model name as input to Model
+        repo (str): the model name as input to Model
         persistent_dir (str): Used to store the downloaded tokenizer
         num_classes (int): The number of output labels/classes
 
     Returns:
         Model: A SequenceClassification instance
     """
-    config = AutoConfig.from_pretrained(model_name,
+    config = AutoConfig.from_pretrained(repo,
                                         cache_dir=persistent_dir / 'cache' / 'transformers')
+    SequenceClassification = huggingface_constructor(config)
 
-    match config.model_type:
-        case 'roberta':
-            with silence_huggingface():
-                return ExtraRoBERTaForSequenceClassification.from_pretrained(
-                    model_name,
-                    num_classes=num_classes,
-                    cache_dir=persistent_dir / 'cache' / 'transformers'
-                )
+    with silence_huggingface():
+        return SequenceClassification.from_pretrained(
+            config=config,
+            num_classes=num_classes,
+            cache_dir=persistent_dir / 'cache' / 'transformers'
+        )
 
-    raise NotImplementedError(f'An embedding abstraction have been implemented for a {config.model_type} model')
+def huggingface_model_from_local(filepath: str) -> Model:
+    """Creates a sequence classification model
+
+    Args:
+        filepath (str): the model name as input to Model
+
+    Returns:
+        Model: A SequenceClassification instance
+    """
+    config = AutoConfig.from_pretrained(filepath)
+    SequenceClassification = huggingface_constructor(config)
+    return SequenceClassification.from_pretrained(
+        config=config,
+        local_files_only=True
+    )
--- experiments/masking.py
+++ experiments/masking.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import pathlib
 import argparse
 import json
@@ -8,10 +7,11 @@ import tempfile
 from timeit import default_timer as timer
 
 import tensorflow as tf
+
 from ecoroar.util import generate_experiment_id, model_name_to_huggingface_repo, default_jit_compile, default_max_epochs
 from ecoroar.dataset import datasets
 from ecoroar.tokenizer import HuggingfaceTokenizer
-from ecoroar.model import HuggingfaceModel
+from ecoroar.model import huggingface_model_from_repo
 from ecoroar.transform import RandomFixedMasking, RandomMaxMasking, BucketedPaddedBatch, TransformSampler
 from ecoroar.optimizer import AdamW
 from ecoroar.scheduler import LinearSchedule
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     # Initialize tokenizer, dataset, and model
     tokenizer = HuggingfaceTokenizer(args.huggingface_repo, persistent_dir=args.persistent_dir)
     dataset = datasets[args.dataset](persistent_dir=args.persistent_dir, seed=args.seed)
-    model = HuggingfaceModel(args.huggingface_repo, persistent_dir=args.persistent_dir, num_classes=dataset.num_classes)
+    model = huggingface_model_from_repo(args.huggingface_repo, persistent_dir=args.persistent_dir, num_classes=dataset.num_classes)
 
     # Load datasets
     dataset_train = dataset.train(tokenizer)
@@ -195,7 +195,6 @@ if __name__ == '__main__':
 
     checkpoint_dir = tempfile.mkdtemp()
     tensorboard_dir = tempfile.mkdtemp()
-
 
     # Train models and collect validation performance at each epoch
     durations['setup'] = timer() - setup_time_start
