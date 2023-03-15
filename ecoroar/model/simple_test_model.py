4278284b96ce31ceb5392d6ffa55d46f3ec71c31
Andreas Madsen <amwebdk@gmail.com>
2023-03-15 12:17:28 -0400

    add hidden_states interface to SimpleTestModel

--- ecoroar/model/simple_test_model.py
+++ ecoroar/model/simple_test_model.py
@@ -1,33 +1,38 @@
 
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Optional
 
 import tensorflow as tf
 
 from ..types import TokenizedDict, EmbeddingDict, Model
 
 _default_embedding = [
-    [0, 1],  # BOS
-    [0, 1],  # EOS
-    [0, 0],  # PAD
-    [1, 0],  # TOKEN
-    [0, 1]   # MASK
+    [0, 1, 0],  # BOS
+    [0, 1, 0],  # EOS
+    [0, 0, 0],  # PAD
+    [1, 0, 0],  # TOKEN
+    [0, 1, 0]   # MASK
 ]
 
 _defalt_kernel = [
     [1, 0, 1],
-    [0, 1, 1]
+    [0, 1, 1],
+    [0, 0, 0]
 ]
 
 @dataclass
 class SimpleOutput():
     logits: tf.Tensor
+    hidden_states: Optional[tf.Tensor] = None
 
 class SimpleTestConfig():
     model_type = 'simple test'
 
     def __init__(self, vocab_size=4) -> None:
         self.vocab_size = vocab_size
+        self.layers = 1
+        self.num_hidden_layers = 1
+        self.hidden_size = 3
 
 class SimpleTestModel(Model):
     def __init__(self,
@@ -57,17 +62,21 @@ class SimpleTestModel(Model):
                 'attention_mask': tf.constant([[1, 1]], dtype=tf.dtypes.int8),
             })
 
-    def call(self, inputs: Union[TokenizedDict, EmbeddingDict], training=False) -> SimpleOutput:
+    def call(self, inputs: Union[TokenizedDict, EmbeddingDict], training=False, output_hidden_states=False) -> SimpleOutput:
         x = inputs
 
         if 'inputs_embeds' not in x:
             x = self.inputs_embeds(x, training=training)
 
-        z = x['inputs_embeds']
-        z = z * z
-        z = tf.math.reduce_sum(z, axis=1)
-        z = self._dense(z, training=training)
-        return SimpleOutput(logits=z)
+        z1 = x['inputs_embeds']
+        z2 = z1 * z1
+        z3 = tf.math.reduce_sum(z2, axis=1)
+        z4 = self._dense(z3, training=training)
+
+        return SimpleOutput(
+            logits=z4,
+            hidden_states=(z1, z2) if output_hidden_states else None
+        )
 
     def inputs_embeds(self, x: TokenizedDict, training=False) -> EmbeddingDict:
         z = self._embedding(x['input_ids'], training=training)
