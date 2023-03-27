d486d6f95a92e5224cf87312d885c6a0ef0fed72
Andreas Madsen <amwebdk@gmail.com>
2023-03-27 15:27:50 -0400

    fix upgrade script

--- upgrade/subdir_results.sh
+++ upgrade/subdir_results.sh
@@ -1,11 +1,11 @@
 
-mkdir -p $SCRATCH/ecoroar/results/masking
-lfs find $SCRATCH/ecoroar/results -name "masking_*" -type f | xargs mv -t $SCRATCH/ecoroar/results/masking
+mkdir -p ~/scratch/ecoroar/results/masking
+lfs find ~/scratch/ecoroar/results  -maxdepth 1 -name "masking_*" -type f | xargs mv -v -t ~/scratch/ecoroar/results/masking
 
-mkdir -p $SCRATCH/ecoroar/results/faithfulness
-lfs find $SCRATCH/ecoroar/results -name "faithfulness_*" -type f | xargs mv -t $SCRATCH/ecoroar/results/faithfulness
-mv  $SCRATCH/ecoroar/intermediate/masked_dataset $SCRATCH/ecoroar/intermediate/faithfulness
+mkdir -p ~/scratch/ecoroar/results/faithfulness
+lfs find ~/scratch/ecoroar/results  -maxdepth 1 -name "faithfulness_*" -type f | xargs mv -v -t ~/scratch/ecoroar/results/faithfulness
+mv  ~/scratch/ecoroar/intermediate/masked_dataset ~/scratch/ecoroar/intermediate/faithfulness
 
-mkdir -p $SCRATCH/ecoroar/results/ood
-lfs find $SCRATCH/ecoroar/results -name "ood_*" -type f | xargs mv -t $SCRATCH/ecoroar/results/ood
-mv  $SCRATCH/ecoroar/intermediate/ood_annotated $SCRATCH/ecoroar/intermediate/ood
+mkdir -p ~/scratch/ecoroar/results/ood
+lfs find ~/scratch/ecoroar/results  -maxdepth 1 -name "ood_*" -type f | xargs mv -v -t ~/scratch/ecoroar/results/ood
+mv  ~/scratch/ecoroar/intermediate/ood_annotated ~/scratch/ecoroar/intermediate/ood
