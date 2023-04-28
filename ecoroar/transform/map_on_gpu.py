47996610c9bdffd1b79e7ba8873a077829d31e90
Andreas Madsen <amwebdk@gmail.com>
2023-04-28 10:35:46 -0400

    fix cardinality propogation in map_on_gpu

--- ecoroar/transform/map_on_gpu.py
+++ ecoroar/transform/map_on_gpu.py
@@ -8,7 +8,7 @@ class MapOnGPU:
         self._mapper = mapper
         self._output_signature = output_signature
 
-    def __call__(self, dataset: tf.data.Dataset) -> tf.data.Dataset:
+    def __call__(self, dataset_in: tf.data.Dataset) -> tf.data.Dataset:
         """Map the dataset using the provided mapper function on the GPU.
 
         Args:
@@ -23,11 +23,20 @@ class MapOnGPU:
         # `with tf.device('GPU')` to force calculations on the GPU is an option too.
         # However, this is still 2x-3x slower, likely because data is not prefetched to
         # the GPU.
-        def _generator():
-            for args in dataset.prefetch(tf.data.AUTOTUNE):
-                yield self._mapper(*args)
+        if isinstance(tf.data.experimental.get_structure(dataset_in), tuple):
+            def _generator():
+                for args in dataset_in.prefetch(tf.data.AUTOTUNE):
+                    yield self._mapper(*args)
+        else:
+            def _generator():
+                for arg in dataset_in.prefetch(tf.data.AUTOTUNE):
+                    yield self._mapper(arg)
 
-        return tf.data.Dataset.from_generator(
+        dataset_out = tf.data.Dataset.from_generator(
             _generator,
-            output_signature=self._output_signature(dataset)
-        ).apply(tf.data.experimental.assert_cardinality(dataset.cardinality()))
+            output_signature=self._output_signature(dataset_in)
+        )
+        cardinality = dataset_in.cardinality()
+        if cardinality >= 0:
+            dataset_out = dataset_out.apply(tf.data.experimental.assert_cardinality(cardinality))
+        return dataset_out
