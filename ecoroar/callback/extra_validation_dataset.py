7403698c7d11ed3b3bababc0e782aaab9c892d1f
Andreas Madsen <amwebdk@gmail.com>
2023-09-26 15:33:33 -0400

    add doc string to ExtraValidationDataset

--- ecoroar/callback/extra_validation_dataset.py
+++ ecoroar/callback/extra_validation_dataset.py
@@ -4,6 +4,19 @@ import tensorflow as tf
 
 class ExtraValidationDataset(tf.keras.callbacks.Callback):
     def __init__(self, *args, name='extra_val', verbose=None, **kwargs):
+        """Enables to run extra validation datasets during training
+
+        These extra validation datasets will be added to the history object.
+        However, they can not be used for early stopping. Make the early
+        stopping dataset the primary validation dataset.
+
+        The arguments to the class initalizer are forwarded to `self.model.evaluate()`,
+        with the exception of `name` which is used to prefix the results in the history object.
+
+        Args:
+            name (str, optional): The name of the validation dataset. Defaults to 'extra_val'.
+            verbose (_type_, optional): Enables verbose mode in `model.evaluate()`. Defaults to None.
+        """
         super().__init__()
         self._name = name
         self._verbose = verbose
